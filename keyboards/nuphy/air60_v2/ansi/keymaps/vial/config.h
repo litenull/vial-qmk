62ae752da3394fd49d236714894a08413bf23ab4
litenull <litenull@users.noreply.github.com>
2025-12-24 16:24:05 +0100

    add nuphy support

---
+++ keyboards/nuphy/air60_v2/ansi/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// Include parent config
+#include "config.h"
+
+// Vial keyboard UID
+#define VIAL_KEYBOARD_UID {0x17, 0x58, 0x52, 0xBC, 0x98, 0xE7, 0x63, 0x23}
+
+// Unlock combo - ESC (row 0, col 0) + Enter (row 3, col 13)
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
